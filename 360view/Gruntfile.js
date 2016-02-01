6e19cc96d8e36a06cb246142533b00d381ae8e64
Vadim Reznik <vadim.reznik@gmail.com>
2016-02-01 18:05:04 +0200

    360view

---
+++ 360view/Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {      
+        mangle: true,
+        compress: true
+      },
+      build: {
+        files: {
+          'app/release/vendors.min.js': [
+			'app/components/jquery/dist/jquery.js', 
+			'app/components/angular/angular.js', 
+			'app/components/angular-file-upload/dist/angular-file-upload.js'
+		  ],
+          'app/release/scripts.min.js': ['app/scripts/**/*.js']
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'app/styles',
+          src: ['*.css', '!*.min.css'],
+          dest: 'app/release',
+          ext: '.min.css'
+        }]
+      }
+    },
+    sass: {
+        dist: {
+          files: {
+          'app/styles/styles.css': [ // в эту папку получиться скомпелированный и сконкатенированный css файл готовый для продакшена
+              'app/styles/styles.scss' // и этого
+            ]
+          }
+        }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss', // следить за изменениями любых файлов с разширениями .scss
+        tasks: ['sass', 'cssmin'] // и запускать такую задачу при их изменении
+      },
+      uglify:{
+        files: 'public/js/<%= pkg.name %>.min.js',
+	      task: ['build']
+      }
+    }
+ 
+  });
+ 
+  //погружаем все необходимые модули
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');    
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+ 
+  //забиваем в задачу по умолчению все наши задачи
+  grunt.registerTask('default', ['uglify', 'cssmin', 'sass','watch']);
+};
