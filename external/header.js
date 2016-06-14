1d0d3121cf9c7b8a90efbf7aaaaed2724e599dc0
Vadim Reznik <vadim.reznik@gmail.com>
2016-06-14 11:29:56 +0300

    external file test

--- external/header.js
+++ external/header.js
@@ -6,10 +6,40 @@
         document = window.document,
         docElem = document.documentElement,
 
-        HeaderABC = {};
+        HeaderABC = {
+            define: function() {
+                this.head = document.head || document.getElementsByTagName('head')[0];
+                this.body = document.body;
+                this.firstBodyElement = this.body.children[0];
+            },
+            generate: {
+                html: function() {
+                    HeaderABC.element = document.createElement('div');
+                    HeaderABC.element.id = 'HeaderABC';
+                    HeaderABC.element.className = 'clearfix';
+                    HeaderABC.element.innerHTML = HeaderABC.template;
+                    HeaderABC.isHtmlGenerated = true;
+                },
+                css: function() {
+                    HeaderABC.styleSheet = document.createElement('style');
+                    HeaderABC.styleSheet.type = 'text/css';
+                    HeaderABC.css = cssArray.join('');
 
-    HeaderABC.template = 
-                        '<div class="header-wrapper">' +
+                    if (HeaderABC.styleSheet.styleSheet) {
+                        HeaderABC.styleSheet.styleSheet.cssText = HeaderABC.css;
+                    } else {
+                        HeaderABC.styleSheet.appendChild(document.createTextNode(HeaderABC.css));
+                    }
+                    HeaderABC.isCssGenerated = true;
+                }
+            },
+            applyMarkup: function() {
+                if (!!this.isHtmlGenerated && !!this.isCssGenerated) {
+                    this.body.insertBefore(HeaderABC.element, this.body.children[0]);
+                    this.head.appendChild(HeaderABC.styleSheet);
+                }
+            },
+            template:   '<div class="header-wrapper">' +
                             '<a href="#" class="hamburger-menu"><span class="icon">=</span></a>' +
                             '<ul class="predefine-actions">' +
                                 '<li>UserName</li>' +
@@ -17,46 +47,28 @@
                             '</ul>' +
                         '</div>' +
                         '<div id="HeaderABC-SidebarMenu">'+
-                        '</div>';
-    var cssArray = [
-        '#HeaderABC', '{ width: 100%; height: 40px; font: 12px/14px Arial, sans-serif; }',
-        '#HeaderABC .header-wrapper', '{ width: 100%; box-sizing: border-box; padding: 10px 20px; }',
-        '#HeaderABC .hamburger-menu', '{ text-decoration: none; }',
-        '#HeaderABC .hamburger-menu .icon', '{ color: red; }',
-        '#HeaderABC .predefine-actions', '{ float: right; }',
-        '#HeaderABC .predefine-actions li', '{ float: left; margin: 0 0 0 10px; }',
-        '#HeaderABC .predefine-actions li a', '{ text-decoration: none;}',
-        '#HeaderABC .clearfix:before', '{ content: " "; display: table; }',
-        '#HeaderABC .clearfix:after', '{ content: " "; display: table; clear: both; }'
-    ];
+                        '</div>',
+            cssArray: [
+                        '#HeaderABC', '{ width: 100%; height: 40px; font: 12px/14px Arial, sans-serif; }',
+                        '#HeaderABC .header-wrapper', '{ width: 100%; box-sizing: border-box; padding: 10px 20px; }',
+                        '#HeaderABC .hamburger-menu', '{ text-decoration: none; }',
+                        '#HeaderABC .hamburger-menu .icon', '{ color: red; }',
+                        '#HeaderABC .predefine-actions', '{ float: right; }',
+                        '#HeaderABC .predefine-actions li', '{ float: left; margin: 0 0 0 10px; }',
+                        '#HeaderABC .predefine-actions li a', '{ text-decoration: none;}',
+                        '#HeaderABC .clearfix:before', '{ content: " "; display: table; }',
+                        '#HeaderABC .clearfix:after', '{ content: " "; display: table; clear: both; }',
+                        '#HeaderABC-SidebarMenu', '{ display: none }'
+            ],
+            initialize: function() {
+                this.generate.html();
+                this.generate.css();
+                this.applyMarkup();
+            }
+        };
 
     document.addEventListener("DOMContentLoaded", function (event) {
-        var head = document.head || document.getElementsByTagName('head')[0],
-            body = document.body;
-        // generate html
-        HeaderABC.element = document.createElement('div');
-        HeaderABC.element.id = 'HeaderABC';
-        HeaderABC.element.className = 'clearfix';
-        HeaderABC.element.innerHTML = HeaderABC.template;
-
-        // apply html to external source
-        body.insertBefore(HeaderABC.element, body.children[0]);
-
-
-
-        // generate stylesheet
-        HeaderABC.styleSheet = document.createElement('style');
-        HeaderABC.styleSheet.type = 'text/css';
-        HeaderABC.css = cssArray.join('');
-
-        if (HeaderABC.styleSheet.styleSheet) {
-            HeaderABC.styleSheet.styleSheet.cssText = HeaderABC.css;
-        } else {
-            HeaderABC.styleSheet.appendChild(document.createTextNode(HeaderABC.css));
-        }
-
-        // apply css to external source
-        head.appendChild(HeaderABC.styleSheet);
+        HeaderABC.initialize();
     });
 
 })(window);
