ee2a2b97dbcef8fce8a655d82ecfd852f6dad8fb
Vadim Reznik <vadim.reznik@gmail.com>
2016-06-29 16:17:18 +0300

    ext_1

--- external/header.js
+++ external/header.js
@@ -98,7 +98,7 @@
                     Utils.html(Utils.getElement('.sidebar-wrapper', HeaderABC.element)[0], HeaderABC.menu);
                     Utils.html(Utils.getElement('.logo', HeaderABC.element)[0], HeaderABC.getTitle());
                     HeaderABC.setUserName('User Name');
-                    HeaderABC.applyResizeEvent();
+                    setTimeout(HeaderABC.applyResizeEvent, 0);
                 }
             },
             applyEvents: function() {
