cb1525d02fa16cb5c331ab8dd62db89ce96d1560
Vadim Reznik <vadim.reznik@gmail.com>
2017-05-29 15:26:04 +0300

    Keys Master System

---
+++ mastersystem/index.js
@@ -0,0 +1,127 @@
+var keys = 4;
+var size = 6;
+var steps = 5;
+
+var min = 2;
+var max = steps;
+
+var pins = new Array(size);
+var count = 0;
+var pin = 0;
+var valid;
+
+var system = {};
+
+function isPinEquals(arr, i){
+	return arr[i - 1] === undefined ? false : arr[i - 1] === arr[i];
+}
+
+function isStepSizeValid(arr, i){
+	return arr[i] >= min && arr[i] <= max;
+}
+
+function isStepsValid(arr, i){
+	return arr[i - 1] === undefined ? true : Math.abs(arr[i - 1] - arr[i]) <= min;
+}
+
+function verify(arr, i){
+	if(isPinEquals(arr, i)){
+		return false;
+	} else if(isStepSizeValid(arr, i) && isStepsValid(arr, i)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function createKey(){
+	while(count < pins.length){
+		pins[count] = pin;
+		valid = verify(pins, count);
+		
+		while(!valid){
+			pins[count]++;
+			valid = verify(pins, count);
+		}
+
+		count++;
+	}
+
+	// if(!system.hasOwnProperty(pins.join(''))){
+	// 	system[pins.join('')] = pins;
+	// } else {
+
+	// }
+}
+
+function isKeyValid(pins){
+	for(var i = 0; i < pins.length; i++){
+		if(!verify(pins, i)){
+			return false;
+		}
+	}
+	return true;
+}
+
+function defineKey(pins){
+	var count = 0;
+	var pin = 0;
+
+	while(count < pins.length){
+		pins[count] = pin;
+		valid = verify(pins, count);
+		
+		while(!valid){
+			pins[count]++;
+			valid = verify(pins, count);
+		}
+
+		count++;
+	}
+	system[pins.join('')] = pins;
+}
+
+function defineKeyFromExists(pins, currentPin){
+	if(currentPin <= pins.length - 1){
+		pins[currentPin]++;
+
+		if(isKeyValid(pins)){
+			return pins;
+		} else {
+			pins[currentPin]--;
+			return defineKeyFromExists(pins, ++currentPin);
+		}
+	} else {
+		return false;
+	}
+}
+
+function buildSystem(){
+	var isExistMoreKeys = true;
+	var pins;
+
+	while(isExistMoreKeys){
+
+		var systemKeys = Object.keys(system);
+		var keySample = !!systemKeys.length ? systemKeys.length - 1 : 0;
+		if(keySample === 0) {
+			defineKey(new Array(size));
+		}
+		systemKeys = Object.keys(system);
+		pins = system[systemKeys[keySample]].slice();
+
+		var extendedPins = defineKeyFromExists(pins, 0);
+
+		isExistMoreKeys = !!extendedPins;
+		if(isExistMoreKeys){
+			if(!system.hasOwnProperty(extendedPins.join(''))){
+				system[extendedPins.join('')] = extendedPins;
+			}
+		}
+	}
+
+}
+debugger;
+buildSystem();
+
+console.log(system);
